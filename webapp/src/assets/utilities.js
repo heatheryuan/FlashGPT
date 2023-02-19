1d18270ac5338ae4a13754cea3effe78e6a679fe
Heather Yuan <heather.yuan@berkeley.edu>
2023-02-19 00:55:21 -0800

    attempting to start database;

---
+++ webapp/src/assets/utilities.js
@@ -0,0 +1,13 @@
+// import {writeJsonFile} from 'write-json-file';
+
+// export default function createSet(data) {
+//     const handleError = (error) => {
+//         if(error) {
+//             console.log(error);
+//             return;
+//         }
+//     }
+    
+//     const jsonStr = JSON.stringify(data, null, 2);
+//     // writeJsonFile('../data/sets.json', jsonStr);
+// }
--- webapp/src/pages/QuickStart.js
+++ webapp/src/pages/QuickStart.js
@@ -1,11 +1,33 @@
 import React, { useState } from "react";
+import createSet from "../assets/utilities";
 
-export default function QuickStart(props) {
+export default function QuickStart() {
+    const [data, setData] = useState();
+    const [notes, setNotes] = useState();
+    const [display, setDisplay] = useState();
+    const [selected, setSelected] = useState(false);
+    const [subject, setSubject] = useState();
+    
+    const handleSubmit = () => {
+        const dataEntry = {
+            subject: subject,
+            notes: notes
+        }
+        setData(dataEntry);
+        createSet(dataEntry);
+        setDisplay(`Subject: ${dataEntry.subject}\nNotes: ${dataEntry.notes}`);
+    }
+
     return (
         <div>
             <h1>Quick Start Flashcards</h1>
             <h3>Copy and paste your notes below to get started!</h3>
-            <input type="text"></input>
+            <form onSubmit={(e) => e.preventDefault()}>
+                <input type='text' onChange={(e) => setSubject(e.target.value)}></input>
+                <input type="text" onChange={(e) => setNotes(e.target.value)}/>
+                <button type='submit' onClick={handleSubmit}> Make flashcards! </button>
+            </form>
+            <p>{display}</p>
         </div>
     )
 }
