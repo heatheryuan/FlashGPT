acd88d89dca2b6e103464eef8aa69a042bc6e041
Heather Yuan <heather.yuan@berkeley.edu>
2023-02-18 19:49:18 -0800

    created basic quick start

---
+++ webapp/src/pages/QuickStart.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function QuickStart(props) {
+    return (
+        <div>
+            <h1>Quick Start Flashcards</h1>
+            <h3>Copy and paste your notes below to get started!</h3>
+            <input type="text"></input>
+        </div>
+    )
+}
